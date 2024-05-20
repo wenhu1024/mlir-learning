a527bb648011ccf3c34466c65577229a90e1a280
wenhu <1340773297@qq.com>
2024-05-20 17:10:58 +0800

    update

---
+++ build/tools/CMakeFiles/tutorial-opt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial-opt.dir/tutorial-opt.cpp.o"
+  "CMakeFiles/tutorial-opt.dir/tutorial-opt.cpp.o.d"
+  "tutorial-opt"
+  "tutorial-opt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial-opt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- learn_02/ctlz.mlir
+++
@@ -1,4 +0,0 @@
-func.func @main(%arg0: i32) -> i32 {
-  %0 = math.ctlz %arg0 : i32
-  func.return %0 : i32
-}
