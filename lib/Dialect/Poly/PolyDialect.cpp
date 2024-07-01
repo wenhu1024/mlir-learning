cdffd18e162eec5e29e357fef4b0436be423d575
wenhu <1340773297@qq.com>
2024-07-01 13:39:53 +0800

    update

--- lib/Dialect/Poly/PolyDialect.cpp
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -1,12 +1,15 @@
 #include "lib/Dialect/Poly/PolyDialect.h"
-#include "lib/Dialect/Poly/PolyTypes.h"
 
+#include "lib/Dialect/Poly/PolyOps.h"
+#include "lib/Dialect/Poly/PolyTypes.h"
 #include "mlir/IR/Builders.h"
 #include "llvm/ADT/TypeSwitch.h"
 
 #include "lib/Dialect/Poly/PolyDialect.cpp.inc"
 #define GET_TYPEDEF_CLASSES
 #include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+#define GET_OP_CLASSES
+#include "lib/Dialect/Poly/PolyOps.cpp.inc"
 
 namespace mlir {
 namespace tutorial {
@@ -16,6 +19,11 @@ void PolyDialect::initialize() {
   addTypes<
 #define GET_TYPEDEF_LIST
 #include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+      >();
+
+  addOperations<
+#define GET_OP_LIST
+#include "lib/Dialect/Poly/PolyOps.cpp.inc"
       >();
 }
 
